e047a2a10159726b3991e85cdd936ddf174c6105
ahmadyaryeet <53474435+ahmadyaryeet@users.noreply.github.com>
2019-08-18 14:29:12 +0100

    Add files via upload

---
+++ cash.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int get_change(float dollar);
+
+int main(void)
+{
+    float dollars;
+    do
+    {
+        dollars = get_float("Change owed: ");
+    }
+    while (dollars < 0);
+    
+    printf("%i\n", get_change(dollars));
+}
+
+int get_change(float dollars)
+    {
+    
+    int cents = round(dollars * 100);
+    int coins = 0;
+    int denom[] =  {25, 10, 5, 1};
+    
+    for (int i = 0; i < 4; i++)
+    {
+        
+        coins += cents / denom[i];
+        cents = cents % denom[i];
+        
+    }
+    return coins;
+}
+        
+    
+    
+    
+    
